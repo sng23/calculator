c1c17315231c51b20ded98fa2a853e9e587c9cbf
sng23 <sng23@njit.edu>
2020-02-29 11:10:28 -0500

    Task: Test for CsvReader.

--- tests/test_CsvReader.py
+++ tests/test_CsvReader.py
@@ -1,14 +1,18 @@
 import unittest
+from Calculator import Calculator
 from CsvReader import CsvReader
 
 
 class CsvReaderTests(unittest.TestCase):
 
     def setUp(self):
-        self.csv_reader = CsvReader('data/Unit Test Addition.csv')
+        self.calculator = Calculator()
 
     def test_instantiate_csvreader(self):
+        self.csv_reader = CsvReader('data/Unit Test CsvReader.txt')
         self.assertIsInstance(self.csv_reader, CsvReader)
+        self.assertEqual(self.csv_reader.data.__len__(), 2)
+
 
 
 if __name__ == '__main__':

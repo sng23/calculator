9b6a595aa6b57744ef4421e3445f93719d33d504
sng23 <sng23@njit.edu>
2020-02-28 20:13:50 -0500

    Task: Move calculator instantiation to setup method.

--- CalculatorTests.py
+++ CalculatorTests.py
@@ -4,13 +4,14 @@ from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.calculator = Calculator()
+        
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
 
-    def test_result(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.result, 7)
+    def test_result(self):        
+        self.assertEqual(self.calculator.result, 7)
 
 
 if __name__ == '__main__':
